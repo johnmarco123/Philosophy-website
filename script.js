a06e14b352c874cccbb43da3246089127604ed7f
johnmarco123 <johnmarcotasillo@gmail.com>
2023-02-14 23:29:20 -0500

    finished book page basically, just have to add details, finished connecting all webpages to each other. Started on quotes page a bit, also started researching into stoicism

--- script.js
+++ script.js
@@ -2,12 +2,31 @@ window.onload=function(){
     var bar = document.getElementById("progress-bar");
     window.addEventListener("scroll", function(){
         //adding px is vital otherwise it will not work properly
-    var totalScroll = window.pageYOffset;
-    var totalHeight = document.body.offsetHeight - window.innerHeight;
-    var amountScrolled = totalScroll / totalHeight; //this is a number between 0 and 1, with 1 being the bottom of the page
-    var totalWidth = document.body.offsetWidth;
+    var body = document.body,
+    html = document.documentElement;
 
-    bar.style.width = totalWidth * amountScrolled + 'px';
+    /***************************************************************************************
+    *    Title: How to get height of entire document with JavaScript?
+    *    Author: Borgar
+    *    Date: Jul 18, 2009
+    *    Code version: 1.0
+    *    Availability: https://stackoverflow.com/questions/1145850/how-to-get-height-of-entire-document-with-javascript
+    *
+    ***************************************************************************************/
+    var height = Math.max( body.scrollHeight, body.offsetHeight, 
+                       html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+    height -= window.innerHeight
+
+    //distance scrolled from 1 - n
+    var totalScroll = window.pageYOffset;
+
+    //distance scrolled between 0 - 1 with 1 being the bottom of the page
+    totalScroll /= height; 
+    var totalWidth = document.body.offsetWidth;
+    bar.style.width = totalWidth * totalScroll + 'px';
+
+    
     })
     
     
